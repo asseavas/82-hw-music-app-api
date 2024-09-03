31fd12aa66d3ade311ba83532f96e84f143d66d6
Klimova Alina <alinaklimova82@gmail.com>
2024-09-03 17:52:13 +0600

    Added logic for getting tracks by artist using query parameters.

--- routes/tracks.ts
+++ routes/tracks.ts
@@ -2,13 +2,24 @@ import express from 'express';
 import { TrackMutation } from '../types';
 import mongoose from 'mongoose';
 import Track from '../models/Track';
+import Album from '../models/Album';
 
 const tracksRouter = express.Router();
 
 tracksRouter.get('/', async (req, res, next) => {
   try {
     const albumId = req.query.album;
-    const query = albumId ? { album: albumId } : {};
+    const artistId = req.query.artist;
+    let query = {};
+
+    if (albumId) {
+      query = { album: albumId };
+    } else if (artistId) {
+      const albums = await Album.find({ artist: artistId }).select('_id');
+      const albumIds = albums.map((album) => album._id);
+      query = { album: { $in: albumIds } };
+    }
+
     const tracks = await Track.find(query);
     return res.send(tracks);
   } catch (error) {
