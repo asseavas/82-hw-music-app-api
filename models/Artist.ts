34079e5291a2d800770729c97c5cb83ccbf125ac
Klimova Alina <alinaklimova82@gmail.com>
2024-09-03 14:04:09 +0600

    A model and schema for the artists has been created. Added a router with routes for creating and receiving artists.

---
+++ models/Artist.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const ArtistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: String,
+  information: String,
+});
+
+const Artist = mongoose.model('Artist', ArtistSchema);
+
+export default Artist;
---
+++ routes/artists.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import Artist from '../models/Artist';
+import mongoose from 'mongoose';
+import { imagesUpload } from '../multer';
+import { ArtistMutation } from '../types';
+
+const artistsRouter = express.Router();
+
+artistsRouter.get('/', async (req, res, next) => {
+  try {
+    const artists = await Artist.find();
+    return res.send(artists);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+artistsRouter.post(
+  '/',
+  imagesUpload.single('image'),
+  async (req, res, next) => {
+    try {
+      const ArtistMutation: ArtistMutation = {
+        name: req.body.name,
+        image: req.file ? req.file.filename : null,
+        information: req.body.information || null,
+      };
+
+      const artist = new Artist(ArtistMutation);
+      await artist.save();
+
+      return res.send(artist);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send(error);
+      }
+
+      return next(error);
+    }
+  },
+);
+
+export default artistsRouter;
