1ef1ae01ff02b9deb509f8d8a5e7c5601fa1d74c
Klimova Alina <alinaklimova82@gmail.com>
2024-09-06 16:49:53 +0600

    Added a router with routes for creating a new user and authenticating him.

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import artistsRouter from './routes/artists';
 import albumsRouter from './routes/albums';
 import tracksRouter from './routes/tracks';
+import usersRouter from './routes/users';
 
 const app = express();
 const port = 8000;
@@ -15,6 +16,7 @@ app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
